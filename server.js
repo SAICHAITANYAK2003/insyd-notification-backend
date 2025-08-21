0eb14623a3048a89742418e43a33d7e34c073302
SAICHAITANYAK2003 <saichaitanyakoduri2003@gmail.com>
2025-08-21 20:24:23 +0530

    cors update

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://insyd-notification-frontend.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
